8771958c78bfdf9aaa619200055f095b70d10784
Roy Hershkovitz <roy.hershkovitz@autodesk.com>
2025-04-06 11:59:38 +0300

    feat: initial project setup with React 17, Rsbuild and TypeScript

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <h1>Welcome to React with Rsbuild</h1>
+      <p>This is a basic React application using React 17, Rsbuild, and TypeScript.</p>
+    </div>
+  );
+};
+
+export default App; 
